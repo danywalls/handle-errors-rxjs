632ef933c8e584b3667dca0b1cf110fb7664a815
danywalls <danywalls4@gmail.com>
2022-03-10 19:25:32 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { BeerService } from './beer.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'my first beer';
+  beers = [];
+  constructor(private beerService: BeerService) {}
+
+  ngOnInit() {
+    try {
+      this.beerService
+        .getBeers()
+        .pipe(catchError(() => of([{ name: 'no beers' }])))
+        .subscribe((beers) => {
+          console.log(beers);
+          this.beers = beers;
+          this.title = beers[0].name;
+        });
+    } catch (err) {
+      this.title = 'Us a error';
+    }
+  }
+}
---
+++ src/app/beer.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BeerService {
+  private apiUrl = 'https://api.punkapi.comzz/v2/beers';
+  constructor(private http: HttpClient) {}
+
+  getBeers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
